18da6466aabe1f7198c60c0a4c1c6adbc34870b5
Adit Jain <aditjain1980@gmail.com>
2022-06-26 21:13:21 +0530

    added files

---
+++ script.js
@@ -0,0 +1,46 @@
+$(".cta").click(function() {
+    $(".form-popup").css("display", "block");
+    $(".body").css("filter","blur(4px)")
+});
+$(".goback").click(function() {
+    $(".form-popup").css("display", "none");
+    $(".body").css("filter","blur(0px)")
+})
+function getFormObj() {
+    var formObj = {};
+    var inputs = $('form').serializeArray();
+    $.each(inputs, function (i, input) {
+        formObj[input.name] = input.value;
+    });
+    return formObj;
+}
+$(".submit").click(function() {
+    if($("input[name='name']").val()==="" || $("input[name='mobile']").val()===""){
+        alert("Please fill in all fields");
+        return;
+    }
+    if(/\D/.test($("input[name='mobile']").val())){
+        alert("Please enter a valid mobile number");
+        return;
+    }
+    
+    $.ajax({
+        method: 'POST',
+        url: 'https://formsubmit.co/8335b3896acd5938113fa50f13c4fda6',
+        dataType: 'json',
+        accepts: 'application/json',
+        data: getFormObj(),
+        success: (data) => { console.log(data);
+            $(".form-popup").css("display", "none");
+            $(".body").css("filter","blur(0px)");
+            $(".thanks").css("display", "block");
+        },
+        error: (err) => {console.log('error', err);
+        $(".form-popup").css("display", "none");
+            $(".body").css("filter","blur(0px)");
+            $(".thanks").show(20);
+            $(".thanks").hide(4000);
+
+    }
+    });
+});
